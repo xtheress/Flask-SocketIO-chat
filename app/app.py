fb9788a15d71cec460300c678b20906185d9071e
civildeath0 <civildeath0@gmail.com>
2021-03-04 23:21:48 +0300

    Refactored old syntax warnings. Added JS for trim spaces and python "if" for deniying empty strings to add in db

--- app/app.py
+++ app/app.py
@@ -2,14 +2,12 @@ from flask import Flask, render_template, url_for, redirect, session, request
 from flask_socketio import SocketIO, send
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very_secret_string'
 socketio = SocketIO(app, cors_allowed_origins='*')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.db'
 db = SQLAlchemy(app)
-
 
 """
 to init db go to console and run:
@@ -30,13 +28,12 @@ class ChatMessages(db.Model):
 
 
 @socketio.on('message')
-def handleMessage(data):
+def handle_message(data):
     print(f"Message: {data}")
     send(data, broadcast=True)
 
     message = ChatMessages(username=data['username'], msg=data['msg'])
-
-    if 'Service message' not in str(message):
+    if 'Service message' not in str(message) and str(data)[9] != "'":
         db.session.add(message)
         db.session.commit()
 
