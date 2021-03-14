5965c6fca5f5aaafc1ad180aa421449654005323
xtheress <@Jhlfjhrjd123>
2021-03-14 22:33:04 +0300

    123123

--- app/app.py
+++ app/app.py
@@ -2,17 +2,16 @@ from flask import Flask, render_template, url_for, redirect, session, request
 from flask_socketio import SocketIO, send
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very_secret_string'
-
 socketio = SocketIO(app, cors_allowed_origins='*')
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_db.db'
 db = SQLAlchemy(app)
 
+
 """
-!!! NOTICE
 to init db go to console and run:
 python
 >>> from app.app import db
@@ -20,24 +19,26 @@ python
 >>> exit()
 """
 
-# 4
+
 class ChatMessages(db.Model):
-    id       = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(256), nullable=False)
-    msg      = db.Column(db.Text, nullable=False)
+    msg = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
 
-# 1
+
 @socketio.on('message')
 def handleMessage(data):
     print(f"Message: {data}")
     send(data, broadcast=True)
 
     message = ChatMessages(username=data['username'], msg=data['msg'])
-    db.session.add(message)
-    db.session.commit()
+
+    if 'Service message' not in str(message):
+        db.session.add(message)
+        db.session.commit()
 
 
 @app.route('/')
